45d38c120264de143cccfd0b16ded947605c73f9
bgpark <bgpark82@gmail.com>
2021-05-22 01:40:45 +0900

    refactoring: AcceptanceTest 분리

--- src/test/java/com/alethio/service/domain/order/acceptance/OrderAcceptanceTest.java
+++ src/test/java/com/alethio/service/domain/order/acceptance/OrderAcceptanceTest.java
@@ -1,29 +1,16 @@
 package com.alethio.service.domain.order.acceptance;
 
+import com.alethio.service.common.AcceptanceTest;
 import com.alethio.service.domain.order.dto.OrderRequest;
-import com.alethio.service.domain.order.dto.OrderRequestStub;
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 
 import static com.alethio.service.domain.order.step.OrderStep.*;
 
 @DisplayName("주문 관련 인수 테스트")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class OrderAcceptanceTest {
-
-    @LocalServerPort
-    int port;
-
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-    }
+public class OrderAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("음식을 주문한다")
     @Test
@@ -37,8 +24,4 @@ public class OrderAcceptanceTest {
         // then
         주문_생성_됨(response);
     }
-
-
-
-
 }
