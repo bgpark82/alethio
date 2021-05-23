ed512495f9651d963a9ff84d349040311f749952
bgpark <bgpark82@gmail.com>
2021-05-24 00:32:23 +0900

    test: 음식 조회 시, 재고가 10개 미만인지 확인 테스트

--- src/test/java/com/alethio/service/domain/item/domain/FoodTest.java
+++ src/test/java/com/alethio/service/domain/item/domain/FoodTest.java
@@ -5,6 +5,7 @@ import com.alethio.service.exception.NoItemLeftException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("음식 관련 도메인 테스트")
@@ -19,5 +20,18 @@ class FoodTest {
         // when then
         assertThatThrownBy(() -> food.decreaseQuantity())
             .isInstanceOf(NoItemLeftException.class);
+    }
+
+    @DisplayName("음식 조회 시, 재고가 10개 미만인지 확인한다")
+    @Test
+    void hasShortStock_IfLessThan10_StockRequest() {
+        // given
+        Food food = ItemStub.of(1L, 9, "떡볶이");
+
+        // when
+        boolean hasEnoughStock = food.hasShortStock();
+
+        // then
+        assertThat(hasEnoughStock).isTrue();
     }
 }
