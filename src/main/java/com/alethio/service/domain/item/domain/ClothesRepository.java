a9b6a4b6b81ac723a46241200e8246af651af5c5
bgpark <bgpark82@gmail.com>
2021-05-25 01:44:35 +0900

    refactoring: Item 엔티티 상속

---
+++ src/main/java/com/alethio/service/domain/item/domain/ClothesRepository.java
@@ -0,0 +1,6 @@
+package com.alethio.service.domain.item.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClothesRepository extends JpaRepository<Clothes, Long> {
+}
